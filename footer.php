d9855465228cb7e134a14c132964ca5338cc44e9
tarz2929 <saitarunmulley@gmail.com>
2020-10-07 11:16:48 -0600

    add footer; imported header and footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <h2><?php bloginfo('title'); ?> </h2>
+    <p>
+    &copy;
+    <?php echo date('Y'); ?>
+    <a href="<?php echo site_url(); ?>">
+        <?php bloginfo( 'title' ); ?>
+    </a>
+    All Rights Reserved
+    </p>
+</footer>
+<?php
+//similar to wp_head()
+    wp_footer();
+?>
+
+</body>
+</html>
